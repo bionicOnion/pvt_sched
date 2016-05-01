b267841cb15823c0f18719d5d17c824e37696bca
Robert Miller <millerrt3@gmail.com>
2016-05-01 00:36:40 -0500

    Initial version of test framework written

--- src/generateTimesliceVector.m
+++ src/generateTimesliceVector.m
@@ -1,42 +1,36 @@
 function [timeslices, unfairness] = generateTimesliceVector(processes, currTime, weight, length)
-%generateTimesliceVector Solve the optimization problem to generate a
-%   timeslice vector
+%generateTimesliceVector Solve for the optimal timeslice vector
 %
 %   Solves a constrained optimization problem based on the provided
-%   variables to generate a vector of timeslices (which will sum to the
-%   provided length variable). This vector can then be used elsewhere to
-%   select the optimal process to schedule next based on whichever policy a
-%   user chooses.
+%     variables to generate a vector of timeslices (which will sum to the
+%     provided length variable). This vector can then be used elsewhere to
+%     select the optimal process to schedule next based on whichever policy
+%     a user chooses.
 %
-%   processes:  A struct array representing processes. Each process has the
-%               following fields:
-%                   count: number of processes
-%                   priorities: vector of integer values; higher values imply
-%                       higher priority
-%                   lastRuns: vector of integer values; time (in milliseconds)
-%                       at which a given process last finished executing
+%   processes:  A struct array representing processes (the fields defined
+%                 within the struct are documented elsewhere)
 %   currTime:   The current time (in milliseconds) at which scheduling is
-%               occuring
+%                 occuring
 %   weighting:  The relative weighting to be applied to the two terms on
-%               the objective function
+%                 the objective function
 %   length:     The desired overall duration of the timeslice vector (e.g.
-%               the vector is constrained such that it sums to this value)
+%                 the vector is constrained such that it sums to this value)
 
     % Perform basic sanity checking on inputs
     if weight < 0 || weight > 1
-        error('generateTimesliceVector:weightOutOfBounds', ...
+        error('pvt_sched:generateTimesliceVector:weightOutOfBounds', ...
             'The provided weight %f is not in the required range of 0 to 1', weight);
     elseif length <= 0 || mod(length,1) ~= 0
-        error('generateTimesliceVector:invalidLength', ...
+        error('pvt_sched:generateTimesliceVector:invalidLength', ...
             'The length of %f is invalid (must be a positive integer', length);
     elseif processes.count <= 0
-        error('generateTimesliceVector:noProcesses', ...
+        error('pvt_sched:generateTimesliceVector:noProcesses', ...
             'The process count of %d was lower than the required minimum of 1', processes.count);
     elseif sum(find(currTime < processes.lastRuns)) ~= 0
-        error('generateTimesliceVector:invalidTimeSequence', ...
+        error('pvt_sched:generateTimesliceVector:invalidTimeSequence', ...
             'One or more processes reported impossible time sequences');
     elseif sum(find(processes.priorities < 0)) ~= 0
-        error('generateTimesliceVector:invalidPriority', ...
+        error('pvt_sched:generateTimesliceVector:invalidPriority', ...
             'One or more processes reported a negative priority');
     end
 
@@ -44,19 +38,26 @@ function [timeslices, unfairness] = generateTimesliceVector(processes, currTime,
     f = @(t)objectiveFunction(t, currTime, weight, length, processes);
     
     % Setup equality constraints such that the timeslice vector sums to the
-    % user-specified total length
+    %   user-specified total length
     eqA = ones(1, processes.count);
     eqB = length;
     
     % Solve for the optimal timeslice values and report the value of the
-    % objective function (the unfairness of the selected schedule)
+    %   objective function (the unfairness of the selected schedule)
+    options = optimset('Display', 'off');
     [timeslices, unfairness] = fmincon(f, zeros(processes.count,1), [], [], eqA, eqB, ...
-        zeros(1, processes.count), length*ones(1, processes.count));
+        zeros(1, processes.count), length*ones(1, processes.count), [], options);
 end
 
 
 function objVal = objectiveFunction(t, s, w, m, processes)
-    prioritization = sum(processes.priorities.*abs(m - t));
+%objectiveFunction The objective function for unfairness minimization
+%
+%   Edits to this function can change/extend the behavior of the scheduler,
+%     making this an avenue for further research/development and user
+%     modification to the scheduler
+
+    prioritization = sum(processes.priorities.*abs(m - t)) / (m/processes.count);
     responsiveness = sum((s - processes.lastRuns)./t);
     
     objVal = w*prioritization + (1-w)*responsiveness;
---
+++ src/runSchedulerSimulation.m
@@ -0,0 +1,97 @@
+function procs = runSchedulerSimulation(numProcesses, simulationLength, varargin)
+%runSchedulerSimulation Simulate the use of the PVT Scheduler
+%
+%   runSchedulerSimulation(numProcesses, simulationLength, varargin)
+%
+%   This function generates a set of processes (specified by numProcesses),
+%     then simulates process secheduling with those processes using the
+%     parameters specified by the user (or default values if none are
+%     provided). When the simulation is complete, the process struct will
+%     be returned so that users can perform any relevant analysis on it.
+%
+%   numProcesses:       The number of processes for which simulation should
+%                         be performed
+%   simulationLength:   The length (in milliseconds) of the simulation
+%   weight:             The weighting to be applied between process
+%                         priority and liveness (default is 0.25)
+%   length:             The desired span covered for every calculation of
+%                         the timeslice vector (default is 100)
+%   priorityMode:       The mode for selecting process priorities. The
+%                         default mode is 'random', which will sample
+%                         priorities from the range 1-10, but 'uniform'
+%                         (for which all priorities will be 0) and
+%                         'ascending' (for which priorities will go from 1
+%                         to numProcesses) are also available options
+%
+%   The process struct contains the following fields:
+%       count: The total number of processes being simulated
+%       lastRuns:       The times at which the processes within the struct
+%                         finished their last timeslice (initialized to
+%                         zeros at the start of the simulation)
+%       priorities:     The scheduling priorities used in the simulation
+%       numTimeSlices:  The total number of times that a given process has
+%                         been scheduled
+%       runTimes:        The total alloted running time for a given process
+
+    % Set scheduling weighting
+    if nargin >= 3 && ~isempty(varargin{1})
+        weight = varargin{1};
+    else
+        weight = 0.25;
+    end
+    
+    % Set timeslice vector target length
+    if nargin >= 4 && ~isempty(varargin{2})
+        length = varargin{2};
+    else
+        length = 100;
+    end
+    
+    % Set process priority mode
+    if nargin >= 5 && ~isempty(varargin{3})
+        priorityMode = varargin{3};
+    else
+        priorityMode = 'random';
+    end
+    
+    % Generate process priorities
+    if strcmp(priorityMode, 'uniform')
+        priorities = zeros(numProcesses,1);
+    elseif strcmp(priorityMode, 'ascending')
+        priorities = (1:numProcesses)';
+    elseif strcmp(priorityMode, 'random')
+        priorities = ceil(rand(numProcesses,1)*10);
+    else
+        error('pvt_sched:runSchedulerSimulator:invalidPriorityMode', ...
+            'The specified priority mode "%s" was not recognized', priorityMode);
+    end
+    
+    % Generate the process struct
+    procs = struct('count', numProcesses);
+    procs.lastRuns = zeros(numProcesses,1);
+    procs.priorities = priorities;
+    procs.numTimeSlices = zeros(numProcesses,1);
+    procs.runTimes = zeros(numProcesses,1);
+    
+    % Run the simulation
+    currTime = 0;
+    while currTime < simulationLength
+        % Solve for the best timeslice/associated process
+        timeslices = generateTimesliceVector(procs, currTime, weight, length);
+        [maxSlice, bestProc] = max(timeslices);
+        sliceTime = ceil(maxSlice);
+        
+        % Update process timing values
+        currTime = currTime + sliceTime;
+        if currTime > simulationLength
+            sliceTime = sliceTime - (currTime - simulationLength);
+            currTime = simulationLength;
+        end
+        procs.lastRuns(bestProc) = currTime;
+        
+        % Perform stat accounting
+        procs.numTimeSlices(bestProc) = procs.numTimeSlices(bestProc) + 1;
+        procs.runTimes(bestProc) = procs.runTimes(bestProc) + sliceTime;
+    end
+end
+
